49fc9ffc8fb7c839bbccc36523424f1d8f5b300f
HuziG <1067408814@qq.com>
2022-08-29 17:32:52 +0800

    update

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,5 +1,17 @@
-declare type Nullable<T> = T | null;
+declare global {
+  const __APP_INFO__: {
+    pkg: {
+      name: string;
+      version: string;
+      dependencies: Recordable<string>;
+      devDependencies: Recordable<string>;
+    };
+    lastBuildTime: string;
+  };
 
-declare type Recordable<T = any> = Record<string, T>;
+  declare type Nullable<T> = T | null;
 
-declare type FormProps<T = any> = Record<string, T>;
+  declare type Recordable<T = any> = Record<string, T>;
+
+  declare type FormProps<T = any> = Record<string, T>;
+}
